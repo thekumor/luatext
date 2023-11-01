3040aea26cedd7732ecd1aaec1969e0a47c592b6
Sebastian Kumór <thekumor@gmail.com>
2023-11-01 09:36:01 +0100

    Ability to read input

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,2 +1,3 @@
 print("Hello!")
+
 lt.System("pause")
--- src/luatext/luatext.c
+++ src/luatext/luatext.c
@@ -3,11 +3,13 @@
 #include <lualib.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 #define FILE_READ_BUFFER_SIZE 65536
+#define INPUT_READ_BUFFER_SIZE 256
 
 /*----------------------------------
 	Prints error to the console.
@@ -15,7 +17,8 @@
 const char* lt_GetError(lua_State* L)
 {
 	const char* error = lua_tostring(L, -1);
-	printf("%s", error);
+	printf("\n%s\n", error);
+	system("pause");
 	return error;
 }
 
@@ -43,7 +46,7 @@ static int LT_Sleep(lua_State* L)
 		luaL_error(L, "%s", "expected number");
 		return 1;
 	}
-	int time = lua_tonumber(L, 1);
+	DWORD time = (DWORD)lua_tonumber(L, 1);
 	Sleep(time);
 
 	return 1;
@@ -78,9 +81,9 @@ static int LT_Color(lua_State* L)
 		luaL_error(L, "%s", "expected number");
 		return 1;
 	}
-	int number = lua_tonumber(L, 1);
+	WORD number = (WORD)lua_tonumber(L, 1);
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(console, (WORD)number);
+	SetConsoleTextAttribute(console, number);
 
 	return 1;
 }
@@ -115,8 +118,8 @@ static int LT_Cursor(lua_State* L)
 	}
 
 	COORD coordinates;
-	coordinates.X = lua_tonumber(L, 1);
-	coordinates.Y = lua_tonumber(L, 2);
+	coordinates.X = (SHORT)lua_tonumber(L, 1);
+	coordinates.Y = (SHORT)lua_tonumber(L, 2);
 
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(console, coordinates);
@@ -146,12 +149,12 @@ static int LT_ReadFile(lua_State* L)
 		return 2;
 	}
 
-	char buffer[FILE_READ_BUFFER_SIZE];
+	char* buffer = (char*)malloc(sizeof(char) * FILE_READ_BUFFER_SIZE);
 
 	char ch = 0;
-	unsigned int i = 0;
+	uint64_t i = 0;
 
-	for (i = 0; ch != EOF && i < FILE_READ_BUFFER_SIZE; i++)
+	for (i = 0; ch != EOF && i < FILE_READ_BUFFER_SIZE - 1; i++)
 	{
 		ch = fgetc(handle);
 		buffer[i] = ch;
@@ -162,6 +165,8 @@ static int LT_ReadFile(lua_State* L)
 	lua_pushstring(L, buffer);
 
 	fclose(handle);
+
+	free(buffer);
 
 	return 1;
 }
@@ -195,6 +200,48 @@ static int LT_SaveFile(lua_State* L)
 	return 2;
 }
 
+/*----------------------------------
+	Retrieves a string from user.
+-----------------------------------*/
+static int LT_ReadString(lua_State* L)
+{
+	const char* msg = 0;
+	if (lua_type(L, 1) == LUA_TSTRING)
+	{
+		msg = lua_tostring(L, 1);
+		printf("%s", msg);
+	}
+
+	char input[INPUT_READ_BUFFER_SIZE] = { 0 };
+
+	fgets(input, INPUT_READ_BUFFER_SIZE, stdin);
+	input[strcspn(input, "\n")] = 0; // Removes the \n
+	lua_pushstring(L, input);
+
+	return 1;
+}
+
+/*----------------------------------
+	Retrieves a number from user.
+-----------------------------------*/
+static int LT_ReadNumber(lua_State* L)
+{
+	const char* msg = 0;
+	if (lua_type(L, 1) == LUA_TSTRING)
+	{
+		msg = lua_tostring(L, 1);
+		printf("%s", msg);
+	}
+
+	char input[INPUT_READ_BUFFER_SIZE] = { 0 };
+	fgets(input, INPUT_READ_BUFFER_SIZE, stdin);
+	double number = atof(input);
+
+	lua_pushnumber(L, number);
+
+	return 1;
+}
+
 static const struct luaL_Reg library[] = {
 #ifdef _WIN32
 	{"Sleep", LT_Sleep},
@@ -205,6 +252,8 @@ static const struct luaL_Reg library[] = {
 	{"System", LT_System},
 	{"ReadFile", LT_ReadFile},
 	{"SaveFile", LT_SaveFile},
+	{"ReadString", LT_ReadString},
+	{"ReadNumber", LT_ReadNumber},
 	{NULL, NULL}
 };
 
